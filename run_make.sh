c5ee287dc9e00333f342e55c87e910e1d4bc2709
ISTweak <istweak@d-o-b.org>
2013-05-02 15:54:45 +0900

    first

---
+++ run_make.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+CCOMPILER=~/work/linaro-arm473/bin/arm-linux-gnueabihf-
+KERNEL_DIR=$PWD
+
+rm -rf modules
+mkdir -p modules
+
+make -j3 ARCH=arm CROSS_COMPILE=$CCOMPILER
+find . -name *.ko -exec cp {} modules \;
+
+cd ../bcm4330_cm9/dhd/linux
+make -j3 ARCH=arm CROSS_COMPILE=$CCOMPILER LINUXDIR=$KERNEL_DIR dhd-cdc-sdmmc-gpl
+find . -name *.ko -exec cp {} $KERNEL_DIR/modules \;
+
+cd $KERNEL_DIR
